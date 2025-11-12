bbf3ce2d1b8935aca8281f86462bb8cd2a3229ed
Lê Ngọc Anh Quân <lnaq.1406.work@gmail.com>
2025-11-12 14:16:27 +0700

    Add CRUD actions to TodoListAPI.html with Axios

--- TodoListAPI/TodoListAPI/obj/Debug/net8.0/TodoListAPI.AssemblyInfo.cs
+++ TodoListAPI/TodoListAPI/obj/Debug/net8.0/TodoListAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoListAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef8cd9ad2bed65fa26f9b1ac4924484404bea6b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de6b06e5844adc1ef8c1cbcf6e8e459ccec18974")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoListAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoListAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
